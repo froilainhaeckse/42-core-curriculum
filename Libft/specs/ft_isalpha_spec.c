98e9e9b0d1899488f1120fef10baf68f5954a389
froilainhaeckse <thea_kupler@riseup.net>
2024-04-17 15:50:51 +0200

    add specs, headers and fr_isalpha

---
+++ Libft/specs/ft_isalpha_spec.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalpha_spec.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkupler <tkupler@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/17 13:34:38 by tkupler           #+#    #+#             */
+/*   Updated: 2024/04/17 15:36:10 by tkupler          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <assert.h>
+#include <ctype.h>
+#include "libft_spec.h"
+
+int	main(void)
+{
+	assert(ft_isalpha('a') == isalpha('a'));
+	assert(ft_isalpha('B') == isalpha('B'));
+	assert(ft_isalpha('1') == isalpha('1'));
+	assert(ft_isalpha(',') == isalpha(','));
+}
