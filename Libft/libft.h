50fe0df7154da236d238dd95c4d7d5acb49fa661
froilainhaeckse <thea_kupler@riseup.net>
2024-04-18 18:07:43 +0200

    add memmove (wip -> check tests)

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: tkupler <tkupler@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 13:43:18 by tkupler           #+#    #+#             */
-/*   Updated: 2024/04/18 12:41:11 by tkupler          ###   ########.fr       */
+/*   Updated: 2024/04/18 16:00:50 by tkupler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,6 @@ int		ft_strlen(char *str);
 void	*ft_memset(void *s, int c, size_t len);
 void	ft_bzero(void *s, size_t len);
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 
 #endif
---
+++ Libft/specs/ft_memmove_spec.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove_spec.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkupler <tkupler@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/18 16:04:03 by tkupler           #+#    #+#             */
+/*   Updated: 2024/04/18 18:03:38 by tkupler          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft_spec.h"
+
+void	ft_memmove_spec(void)
+{
+
+	char s[] = {65, 66, 67, 68, 69, 0, 45};
+    char s0[] = {0, 0, 0, 0, 0, 0, 0};
+    char sCpy[] = {65, 66, 67, 68, 69, 0, 45};
+    char sResult[] = {67, 68, 67, 68, 69, 0, 45};
+    char sResult2[] = {67, 67, 68, 68, 69, 0, 45};
+
+    assert(ft_memmove(s0, s, 7) == s0 && memcmp(s, s0, 7) == 0);
+
+    assert(ft_memmove(s, s + 2, 0) && memcmp(s, sCpy, 7) == 0);
+
+    assert(ft_memmove(s, s + 2, 2) == s && memcmp(s, sResult, 7) == 0);
+
+    assert(ft_memmove(sResult + 1, sResult, 2) == sResult + 1 && memcmp(sResult, sResult2, 7) == 0);
+	    // Non-overlapping memory regions
+    {
+        char s[] = "Hello, World!";
+        char s0[sizeof(s)] = {0};
+        assert(ft_memmove(s0, s, strlen(s)) == s0 && memcmp(s, s0, strlen(s)) == 0);
+    }
+
+    // Overlapping memory regions (src < dest)
+    {
+        char s[] = "Hello, World!";
+        assert(ft_memmove(s + 2, s, strlen(s) - 2) == s + 2 && memcmp(s, "HeHello, World!", strlen(s)) == 0);
+    }
+
+    // Overlapping memory regions (src > dest)
+    {
+        char s[] = "Hello, World!";
+        assert(ft_memmove(s - 2, s, strlen(s) - 2) == s && memcmp(s, "llo, World!", strlen(s)) == 0);
+    }
+	// Moving zero bytes
+	{
+    char s[] = "Hello, World!";
+    char sCpy[sizeof(s)];
+    memcpy(sCpy, s, sizeof(s));
+    assert(ft_memmove(s, s, 0) == s && memcmp(s, sCpy, sizeof(s)) == 0);
+    }
+
+	// Moving from/to NULL pointers
+	assert(ft_memmove(NULL, NULL, 0) == NULL);
+
+    printf("ft_memmove tests passed!\n");
+}
