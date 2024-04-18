684fca10b8e10024b256bc51a9cbf284c5f239a7
froilainhaeckse <thea_kupler@riseup.net>
2024-04-18 12:10:05 +0200

    include lib for header to include size_t

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,18 +6,21 @@
 /*   By: tkupler <tkupler@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 13:43:18 by tkupler           #+#    #+#             */
-/*   Updated: 2024/04/18 11:32:48 by tkupler          ###   ########.fr       */
+/*   Updated: 2024/04/18 11:57:56 by tkupler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-int	ft_strlen(char *str);
+# include <unistd.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_strlen(char *str);
+void	*ft_memset(void *b, int c, size_t len);
 
 #endif
