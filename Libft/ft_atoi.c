99c2c55371083d46c51870a4bd67696e571788b2
froilainhaeckse <thea_kupler@riseup.net>
2024-04-29 18:57:58 +0200

    fix atoi

--- Libft/ft_atoi.c
+++ Libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: tkupler <tkupler@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 18:31:28 by tkupler           #+#    #+#             */
-/*   Updated: 2024/04/19 18:35:36 by tkupler          ###   ########.fr       */
+/*   Updated: 2024/04/29 18:55:22 by tkupler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,11 @@ int	ft_atoi(const char *str)
 {
 	int	sign;
 	int	result;
+	int	sign_counter;
 	int	i;
 
 	sign = 1;
+	sign_counter = 0;
 	i = 0;
 	result = 0;
 	while (ft_isspace(str[i]))
@@ -39,8 +41,11 @@ int	ft_atoi(const char *str)
 	{
 		if (str[i] == '-')
 			sign *= -1;
+		sign_counter++;
 		i++;
 	}
+	if (sign_counter > 1)
+		return (0);
 	while (ft_isdigit(str[i]))
 	{
 		result = result * 10 + (str[i] - '0');
--- Libft/specs/ft_atoi_spec.c
+++ Libft/specs/ft_atoi_spec.c
@@ -6,7 +6,7 @@
 /*   By: tkupler <tkupler@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 18:22:30 by tkupler           #+#    #+#             */
-/*   Updated: 2024/04/19 18:37:03 by tkupler          ###   ########.fr       */
+/*   Updated: 2024/04/29 18:49:38 by tkupler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,10 @@ void	ft_atoi_spec(void)
     assert(ft_atoi("42 is the answer") == atoi("42 is the answer"));
     assert(ft_atoi("   789") == atoi("   789"));
     assert(ft_atoi("abc123") == atoi("abc123"));
+    assert(ft_atoi("--123") == atoi("--123"));
+    assert(ft_atoi("-+123") == atoi("-+123"));
+    assert(ft_atoi("+-123") == atoi("+-123"));
+    assert(ft_atoi("++123") == atoi("++123"));
     
     printf("ft_atoi test passed!\n");
 }
