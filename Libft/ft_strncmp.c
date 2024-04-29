1d1becd4a0fd6ad1349afaa6463588b50fa80fa9
froilainhaeckse <thea_kupler@riseup.net>
2024-04-29 18:57:33 +0200

    fix strncmp

--- Libft/ft_strncmp.c
+++ Libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: tkupler <tkupler@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 22:27:19 by tkupler           #+#    #+#             */
-/*   Updated: 2024/04/19 18:11:16 by tkupler          ###   ########.fr       */
+/*   Updated: 2024/04/29 15:59:03 by tkupler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,22 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
+	unsigned const char	*str1;
+	unsigned const char	*str2;
 	size_t	i;
 
+	str1 = (unsigned const char *)s1;
+	str2 = (unsigned const char *)s2;
 	i = 0;
-	if (n <= 0)
-		return (0);
-	while (s1[i] != '\0' && s2[i] != '\0' && i < n)
+	while (i < n && str1[i])
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		if (str1[i] != str2[i])
+			return (int)(str1[i] - str2[i]);
 		i++;
 	}
-	if (i == n)
+	if (n != 0 && i == n)
+		return (str1[n - 1] - str2[n - 1]);
+	else if (n == 0)
 		return (0);
-	return (s1[i] - s2[i]);
+	return (str1[i] - str2[i]);
 }
--- Libft/specs/ft_strncmp_spec.c
+++ Libft/specs/ft_strncmp_spec.c
@@ -6,7 +6,7 @@
 /*   By: tkupler <tkupler@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 18:22:30 by tkupler           #+#    #+#             */
-/*   Updated: 2024/04/19 18:08:12 by tkupler          ###   ########.fr       */
+/*   Updated: 2024/04/29 15:58:35 by tkupler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ void	ft_strncmp_spec(void)
 	assert(ft_strncmp(str1, str2, 8) == strncmp(str1, str2, 8));
 	assert(ft_strncmp(str1, str2, 12) == strncmp(str1, str2, 12));
 	assert(ft_strncmp(str1, str3, 13) == strncmp(str1, str3, 13));
+	assert(ft_strncmp("abcdef", "abc\375xx", 5) < 0);
 
 	printf("ft_strncmp test passed!\n");
 }
