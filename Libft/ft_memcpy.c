46993c43d2a082891a0c51d5c26c30b00893322a
froilainhaeckse <thea_kupler@riseup.net>
2024-05-06 15:10:50 +0200

    include edge cases for memcpy

--- Libft/ft_memcpy.c
+++ Libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: tkupler <tkupler@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 12:17:38 by tkupler           #+#    #+#             */
-/*   Updated: 2024/05/06 13:05:37 by tkupler          ###   ########.fr       */
+/*   Updated: 2024/05/06 14:17:18 by tkupler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,19 @@ void	*ft_memcpy(void *restrict dest, const void *restrict src, size_t n)
 	const char	*s;
 	size_t		i;
 
-	if (dest == NULL || src == NULL)
+	if (dest == NULL)
 		return (NULL);
+	if (n == 0)
+		return (dest);
 	i = 0;
 	s = src;
 	d = dest;
 	while (i < n)
 	{
-		d[i] = s[i];
+		if (src == NULL)
+			d[i] = '\0';
+		else
+			d[i] = s[i];
 		i++;
 	}
 	return (dest);
