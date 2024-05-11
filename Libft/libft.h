5d7d168f52454b284ebc9fc8e0f914bcd248bc68
froilainhaeckse <thea_kupler@riseup.net>
2024-05-11 10:12:41 +0200

    add strmapi

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: tkupler <tkupler@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 13:43:18 by tkupler           #+#    #+#             */
-/*   Updated: 2024/05/09 12:16:52 by tkupler          ###   ########.fr       */
+/*   Updated: 2024/05/09 13:40:14 by tkupler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,5 +46,7 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned
+int, char));
 
 #endif
---
+++ Libft/specs/ft_strmapi_spec.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi_spec.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkupler <tkupler@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/17 18:22:30 by tkupler           #+#    #+#             */
+/*   Updated: 2024/05/09 13:45:44 by tkupler          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft_spec.h"
+
+void ft_strmapi_spec(void)
+{
+    // char *result1 = ft_itoa(1);
+    // char *expected1 = "1";
+    // assert(strcmp(result1, expected1) == 0);
+    // free(result1);
+
+    // char *result2 = ft_itoa(2233);
+    // char *expected2 = "2233";
+    // assert(strcmp(result2, expected2) == 0);
+    // free(result2);
+
+    printf("ft_strmapi test passed!\n");
+}
